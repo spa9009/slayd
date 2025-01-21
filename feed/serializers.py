af3a63897e46b7e1ddd91fe6154bd23fc92ba269
sparsh <sparshjain2000@gmail.com>
2025-01-21 19:15:23 +0530

    Added post id field in the product data

--- feed/serializers.py
+++ feed/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from .models import Product, Media, Post, TaggedProduct
 
+
+## TODO: This might increase the latency of the similar posts API while fetching the products in the post. 
+## Can be solved by creating a new API which can be triggered from PDP. 
 class ProductSerializer(serializers.ModelSerializer):
+    post_id = serializers.SerializerMethodField()  # Method field for the related Post ID
     class Meta:
         model = Product
-        fields = ['id', 'category', 'subcategory', 'brand', 'name', 'image_url', 'secondary_images', 'product_link', 'price', 'discount_price', 'description', 'gender']
-        read_only_fields = ['created_at']
+        fields = [
+            'id', 'category', 'subcategory', 'brand', 'name', 'image_url', 'secondary_images', 'product_link', 'price', 'discount_price', 
+            'description', 'gender', 'post_id', 'created_at'
+        ]
+        read_only_fields = ['created_at', 'post_id'] 
+
+    def get_post_id(self, obj):
+        post = obj.posts.first()
+        return post.id if post else None
 
     def create(self, validated_data):
-        # Creating the product instance with only validated data fields
-        return Product.objects.create(**validated_data)
+        return super().create(validated_data)
 
 class MediaSerializer(serializers.ModelSerializer):
     class Meta:
