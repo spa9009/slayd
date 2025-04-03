4129404d96d24eb3313f8071fff8d3a3756cf10c
sparsh <sparshjain2000@gmail.com>
2025-04-03 17:16:09 +0530

    Added health check endpoint

--- slayd/urls.py
+++ slayd/urls.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import JsonResponse
+
+def health_check(request):
+    return JsonResponse({"status": "ok"}, status=200)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,4 +11,5 @@ urlpatterns = [
     path('account/', include('account.urls')),
     path('activity/', include('activity.urls')),
     path('instagram/', include('instagram_bot.urls')),
+    path("health/", health_check)
 ]
